1024dea7dd8344adfcb302cbf457c59feb5b8125
Aleksandr_Volkov <guthub.volkov@gmail.com>
2021-03-19 14:19:48 +0300

    Задеплоил для тестов, ссылка в Readme

--- src/redux/weatherReducer.js
+++ src/redux/weatherReducer.js
@@ -33,21 +33,7 @@ export const setLoading = ()=>({
   type:SWITCH_LOADING
 })
 
-// export const setCoord = (data) => ({
-//   type: SET_COORD,
-//   value: data,
-// });
 
-// export const getWeather = () => {
-//   return (dispatch) => {
-//     const url =
-//       "https://api.openweathermap.org/data/2.5/weather?q=London&appid=ac064332e753f882489837dae6bcd333";
-
-//     axios.get(url).then((response) => {
-//       dispatch(setLoadData(response.data));
-//     });
-//   };
-// };
 
 export const getPosition = () => {
   return (dispatch) => {
