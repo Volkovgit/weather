482103afd2e00878ded4f6678235459a31b37208
volkovgit <volkovgit@gmail.com>
2022-06-30 23:11:41 +0300

    начал верстать макет

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore,applyMiddleware } from 'redux'
-import {weatherReducer} from './weatherReducer'
-import thunk from 'redux-thunk';
-const store = createStore(weatherReducer,applyMiddleware(thunk));
-
-export default store;
