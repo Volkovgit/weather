f9fa8b9dca1f41afe571526ca7d6e1874f49735a
Aleksandr Volkov <guthub.volkov@gmail.com>
2023-03-25 14:14:04 +0300

    Начал исправлять верстку проекта и переводить его на ангуляр

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weather-app';
+  location = 'Yaroslavl';
+  today = 'Today, October 15 5:45'
+  temperature='23°'
 }
